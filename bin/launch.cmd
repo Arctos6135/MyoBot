6c3267d54fe8d6e95784acdfb5b3dca57780760a
Tyler Tian <tylertian123@gmail.com>
2018-05-13 18:28:08 -0400

    Create launch file

---
+++ bin/launch.cmd
@@ -0,0 +1,6 @@
+REM First start the Myo reader
+start myo_reader.exe
+REM Delay a bit for the Myo reader program to set up the sockets
+ping localhost -n 1 -w 500 > nul
+REM Now start the Java program in a new window and keep it open
+start cmd /c "java -jar bridge.jar"
