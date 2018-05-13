9977ce4b87b0af18d67340f683396432a73e7e73
Tyler Tian <tylertian123@gmail.com>
2018-05-13 14:58:56 -0400

    Start C++ side program

---
+++ MyoBot_Cpp/MyoBot_Cpp/MyoBot_Cpp/stdafx.h
@@ -0,0 +1,22 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <iostream>
+#include <tchar.h>
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+// TODO: reference additional headers your program requires here
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <Windows.h>
+#include <string>
+#include <stdexcept>
