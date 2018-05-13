ec457124ee4b934f31ba62a759a16a71c5bdf063
Tyler Tian <tylertian123@gmail.com>
2018-05-13 14:20:33 -0400

    Add to bridge program

--- MyoBot_Java/src/myobot/bridge/Main.java
+++ MyoBot_Java/src/myobot/bridge/Main.java
@@ -2,6 +2,7 @@ package myobot.bridge;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.ConnectException;
 import java.net.Socket;
 
 import edu.wpi.first.networktables.NetworkTable;
@@ -15,6 +16,16 @@ public class Main {
 	static NetworkTableEntry driveForwardEntry;
 	
 	public static final int PORT = 6135;
+	
+	//Messages are all 4 bytes
+	public static final int MSG_REST = 0x0000;
+	public static final int MSG_DRIVEFORWARD = 0x0001;
+	
+	public static int chars2Int(char[] data) {
+		if(data.length < 4)
+			throw new IllegalArgumentException("Not enough bytes");
+		return ((byte) data[0] << 24) | ((byte) data[1] << 16) | ((byte) data[2] << 8) | (byte) data[3];
+	}
 
 	public static void main(String[] args) {
 		tableInstance = NetworkTableInstance.getDefault();
@@ -29,13 +40,35 @@ public class Main {
 			Socket socket = new Socket("localhost", PORT);
 			InputStreamReader in = new InputStreamReader(socket.getInputStream());
 			
-			//TODO: Put code here to handle messages
+			char[] buf = new char[4];
+			while(in.read(buf) != -1) {
+				int msg = chars2Int(buf);
+				switch(msg) {
+				//Set all actions to false
+				case MSG_REST:
+					driveForwardEntry.setBoolean(false);
+					break;
+				case MSG_DRIVEFORWARD:
+					driveForwardEntry.setBoolean(true);
+					break;
+				//Set all actions to false and report error
+				default:
+					driveForwardEntry.setBoolean(false);
+					System.err.println("Error: Unrecognized action code received");
+					break;
+				}
+			}
 			
 			in.close();
 			socket.close();
 		}
 		catch(IOException e) {
-			System.err.println("Fatal Error: " + e.toString());
+			if(e instanceof ConnectException) {
+				System.err.println("Failed to connect. Please run the Myo reader before running the bridge.");
+			}
+			else {
+				System.err.println("Fatal Error: " + e.toString());
+			}
 		}
 	}
 
