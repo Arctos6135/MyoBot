8a3cf87248ea7b60cb978ebab4cb136207f9a278
Tyler Tian <tylertian123@gmail.com>
2018-06-11 20:59:33 -0400

    Add comments to bridge program

--- MyoBot_Java/src/myobot/bridge/Main.java
+++ MyoBot_Java/src/myobot/bridge/Main.java
@@ -11,14 +11,28 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
+/**
+ * This class is the bridge between the native Myo reader program and the robot.
+ * The Myo reader sends data to this program over a socket, and the program re-sends that data over
+ * to the robot using NetworkTables.
+ * @author Tyler Tian
+ *
+ */
 public class Main {
+	//The names of the tables and table entries
+	public static final String TABLE_NAME = "myobot";
+	public static final String ACTION_ENTRY_NAME = "action";
+	public static final String PARAM_ENTRY_NAME = "param";
 	
+	//NetworkTables instance and table
 	static NetworkTableInstance tableInstance;
 	static NetworkTable table;
 	
+	//The entry that stores the action is a single value, while the parameter entries are an array of 4
 	static NetworkTableEntry actionEntry;
 	static NetworkTableEntry[] paramEntries;
 	
+	//This map matches action codes to their corresponding names so we can output the name of the action
 	static HashMap<Integer, String> actionNames;
 	static {
 		actionNames = new HashMap<Integer, String>();
@@ -35,11 +49,13 @@ public class Main {
 	
 	public static final int PORT = 6135;
 	
+	//Used later to convert the raw bytes from the socket to an integer action code
 	public static int chars2Int(char[] data) {
 		if(data.length < 4)
 			throw new IllegalArgumentException("Not enough bytes");
 		return ((byte) data[0] << 24) | ((byte) data[1] << 16) | ((byte) data[2] << 8) | (byte) data[3];
 	}
+	//Used later to convert the array of bytes from the socket to a hexadecimal representation
 	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
 	public static String bytesToHex(byte[] bytes) {
 	    char[] hexChars = new char[bytes.length * 2];
@@ -51,14 +67,18 @@ public class Main {
 	    return new String(hexChars);
 	}
 	
+	//Length of last message
+	//Used to overwrite last message
 	static int lastMessageLen = 0;
 
 	public static void main(String[] args) {
+		//Prompt for team number, which is used later for the NetworkTables connection.
 		Scanner stdin = new Scanner(System.in);
 		System.out.print("Enter FRC Team Number: ");
 		int teamNumber = stdin.nextInt();
 		stdin.close();
 		
+		//Create tables and entries
 		tableInstance = NetworkTableInstance.getDefault();
 		table = tableInstance.getTable("myobot");
 		
@@ -67,6 +87,8 @@ public class Main {
 			paramEntries[i] = table.getEntry("param" + i);
 		}
 		
+		//Default update rate is too slow
+		//Change to 20 times a second instead
 		tableInstance.setUpdateRate(1.0 / 20);
 		tableInstance.startClientTeam(teamNumber);
 		tableInstance.startDSClient();
@@ -78,21 +100,27 @@ public class Main {
 			char[] buf = new char[8];
 			char[] actionBytes = new char[4];
 			byte[] param = new byte[4];
+			//Read the socket message, in 8 raw bytes
 			while(in.read(buf) != -1) {
+				//Separate the 8 bytes into bytes for the action code and param
 				for(int i = 0; i < 4; i ++) {
 					actionBytes[i] = buf[i];
 					param[i] = (byte) buf[i + 4];
 				}
 				int action = chars2Int(actionBytes);
 				
+				//Set the values to send them over NetworkTables
 				actionEntry.forceSetNumber(action);
 				for(int i = 0; i < 4; i ++) {
 					paramEntries[i].forceSetValue(param[i]);
 				}
 				
+				//Output information
 				String message = "Action Sent: " + Integer.toHexString(action) + "(" +
 						(actionNames.containsKey(action) ? actionNames.get(action) : "Unknown") + ")\n" +
 						"Parameter Data: 0x" + bytesToHex(param);
+				//Output backspace characters to erase the last line before outputting our new message.
+				//This makes sure the line is cleared
 				for(int i = 0; i < lastMessageLen; i ++) {
 					System.out.print("\b");
 				}
