d575f20c58aec6a0e83e31a70faa65a093992ece
Tyler Tian <tylertian123@gmail.com>
2018-06-17 01:10:58 -0400

    Clean up binaries

--- bin/launch.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-REM First start the Myo reader
-start myo_reader.exe
-REM Delay a bit for the Myo reader program to set up the sockets
-ping localhost -n 1 -w 500 > nul
-REM Now start the Java program in a new window and keep it open
-start cmd /c "title MyoBot Bridge Program && java -jar bridge.jar"
