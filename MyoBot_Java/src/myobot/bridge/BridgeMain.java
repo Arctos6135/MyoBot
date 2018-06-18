983fe7878c3a2d27085ef60b6b507a62242ae042
Tyler Tian <tylertian123@gmail.com>
2018-06-18 15:08:29 -0400

    Clean up UI and add border to connect to myo dialog

--- MyoBot_Java/src/myobot/bridge/BridgeMain.java
+++ MyoBot_Java/src/myobot/bridge/BridgeMain.java
@@ -5,13 +5,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Image;
-import java.awt.Window;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.ExecutionException;
-import java.util.stream.Stream;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
@@ -26,7 +24,6 @@ import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
-import javax.swing.SwingWorker;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -47,17 +44,17 @@ public class BridgeMain {
 	//Main JFrame
 	static JFrame mainFrame;
 	//Panel that stores the top status icons and buttons
-	static JPanel iconsAndButtonsPanel;
+	static JPanel topBarPanel;
 	//Buttons in the top bar
 	static JButton lockUnlockButton;
 	static JButton invertButton;
 	//Different icons
-	static ImageIcon unlockStatusIcon, onArmStatusIcon, invertedStatusIcon;
+	static ImageIcon unlockStatusIcon, onArmStatusIcon, invertStatusIcon;
 	//Last time's status
 	//Used to determine whether or not to update the icons
 	static boolean lastUnlocked = false, lastOnArm = false, lastInverted = invertAngles;
 	//The "connecting to myo" dialog
-	static JDialog connectingToMyoDialog;
+	static JDialog connectingDialog;
 	//Different icon images
 	static Image iconLocked, iconUnlocked, iconOnArm, iconOffArm, iconNormal, iconInverted;
 	
@@ -114,26 +111,33 @@ public class BridgeMain {
 		mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
 		//"Connecting to Myo" Dialog
-		connectingToMyoDialog = new JDialog(mainFrame, "Please Wait...");
-		connectingToMyoDialog.setModal(true);
-		connectingToMyoDialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-		connectingToMyoDialog.setLayout(new BorderLayout());
+		connectingDialog = new JDialog(mainFrame, "Please Wait...");
+		connectingDialog.setModal(true);
+		connectingDialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+		JPanel connectingDialogPanel = new JPanel();
+		connectingDialogPanel.setBorder(BorderFactory.createLineBorder(new Color(10, 10, 10), 1, true));
+		connectingDialogPanel.setLayout(new BorderLayout());
 		JLabel l = new JLabel("Connecting to Myo...");
 		l.setFont(l.getFont().deriveFont(18.0f));
 		l.setHorizontalAlignment(JLabel.CENTER);
 		l.setVerticalAlignment(JLabel.CENTER);
+		//Create an empty border around the label
 		l.setBorder(BorderFactory.createEmptyBorder(20, 25, 20, 25));
-		connectingToMyoDialog.add(l, BorderLayout.CENTER);
+		connectingDialogPanel.add(l, BorderLayout.CENTER);
+
 		JProgressBar connectionProgress = new JProgressBar();
 		connectionProgress.setPreferredSize(new Dimension(350, 30));
 		connectionProgress.setIndeterminate(true);
+		//The progress bar has its own panel, because directly adding a border to it doesn't work
 		JPanel progressBarPanel = new JPanel();
 		progressBarPanel.add(connectionProgress);
 		progressBarPanel.setBorder(BorderFactory.createEmptyBorder(0, 25, 20, 25));
-		connectingToMyoDialog.add(progressBarPanel, BorderLayout.PAGE_END);
-		connectingToMyoDialog.setUndecorated(true);
-		connectingToMyoDialog.pack();
-		connectingToMyoDialog.setLocationRelativeTo(null);
+		connectingDialogPanel.add(progressBarPanel, BorderLayout.PAGE_END);
+		connectingDialog.setContentPane(connectingDialogPanel);
+		connectingDialog.setUndecorated(true);
+		connectingDialog.pack();
+		//Center the dialog
+		connectingDialog.setLocationRelativeTo(null);
 		
 		//Load images
 		mainFrame.setSize(new Dimension(500, 500));
@@ -146,18 +150,15 @@ public class BridgeMain {
 		
 		//Top bar with the icons and buttons
 		mainFrame.setLayout(new BoxLayout(mainFrame.getContentPane(), BoxLayout.Y_AXIS));
-		iconsAndButtonsPanel = new JPanel();
+		topBarPanel = new JPanel();
+		//Construct ImageIcons
 		unlockStatusIcon = new ImageIcon(iconLocked);
 		onArmStatusIcon = new ImageIcon(iconOffArm);
-		invertedStatusIcon = new ImageIcon(invertAngles ? iconInverted : iconNormal);
-		iconsAndButtonsPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 15, 5));
-		iconsAndButtonsPanel.add(new JLabel(unlockStatusIcon));
-		iconsAndButtonsPanel.add(new JLabel(onArmStatusIcon));
-		iconsAndButtonsPanel.add(new JLabel(invertedStatusIcon));
-		JPanel topButtonsPanel = new JPanel();
-		lockUnlockButton = new JButton("Unlock");
-		lockUnlockButton.setPreferredSize(new Dimension(80, 30));
-		lockUnlockButton.addActionListener(e -> {
+		invertStatusIcon = new ImageIcon(invertAngles ? iconInverted : iconNormal);
+		topBarPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 15, 5));
+		//Store these lambdas as runnables
+		//Reused later
+		Runnable lockUnlockMyo = () -> {
 			if(myo.isLocked()) {
 				myo.unlock();
 				lockUnlockButton.setText("Lock");
@@ -168,24 +169,63 @@ public class BridgeMain {
 				lockUnlockButton.setText("Unlock");
 				unlockStatusIcon.setImage(iconLocked);
 			}
-			iconsAndButtonsPanel.revalidate();
-			iconsAndButtonsPanel.repaint();
+			topBarPanel.revalidate();
+			topBarPanel.repaint();
+		};
+		Runnable invertUninvertAngles = () -> {
+			invertAngles = !invertAngles;
+			invertStatusIcon.setImage(invertAngles ? iconInverted : iconNormal);
+			topBarPanel.revalidate();
+			topBarPanel.repaint();
+		};
+		
+		//Add our ImageIcons to the top bar
+		JLabel unlockStatusLabel = new JLabel(unlockStatusIcon);
+		//Make them clickable
+		unlockStatusLabel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				lockUnlockMyo.run();
+			}
+		});
+		topBarPanel.add(unlockStatusLabel);
+		topBarPanel.add(new JLabel(onArmStatusIcon));
+		JLabel invertedStatusLabel = new JLabel(invertStatusIcon);
+		invertedStatusLabel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				invertUninvertAngles.run();
+			}
+		});
+		topBarPanel.add(invertedStatusLabel);
+		
+		//Buttons have their own sub-panel so they can have different gaps
+		JPanel topButtonsPanel = new JPanel();
+		lockUnlockButton = new JButton("Unlock");
+		lockUnlockButton.setPreferredSize(new Dimension(80, 30));
+		lockUnlockButton.addActionListener(e -> {
+			lockUnlockMyo.run();
 		});
 		invertButton = new JButton("Invert");
 		invertButton.setPreferredSize(new Dimension(80, 30));
+		invertButton.addActionListener(e -> {
+			invertUninvertAngles.run();
+		});
 		topButtonsPanel.add(lockUnlockButton);
 		topButtonsPanel.add(invertButton);
-		iconsAndButtonsPanel.add(topButtonsPanel);
+		topBarPanel.add(topButtonsPanel);
 		
-		mainFrame.add(iconsAndButtonsPanel);
+		mainFrame.add(topBarPanel);
 		
 		mainFrame.setVisible(true);
 		
+		//Prompt for team number
 		JPanel promptPanel = new JPanel();
 		promptPanel.setLayout(new BorderLayout());
 		promptPanel.add(new JLabel("Enter FRC Team Number, or 0 for Dry-Run:"), BorderLayout.CENTER);
 		JTextArea teamNumber = new JTextArea();
 		promptPanel.add(teamNumber, BorderLayout.PAGE_END);
+		//Initialize NT
 		ntInstance = NetworkTableInstance.getDefault();
 		ntTable = ntInstance.getTable("myobot");
 		ntDriveLeft = ntTable.getEntry("leftDrive");
@@ -210,7 +250,7 @@ public class BridgeMain {
 		}
 		
 		uiIsSetUp = true;
-		connectingToMyoDialog.setVisible(true);
+		connectingDialog.setVisible(true);
 	}
 	
 	public static void updateUI() {
@@ -262,8 +302,8 @@ public class BridgeMain {
 		}
 		myo.setLockingPolicy(Myo.LOCKING_POLICY_NONE);
 		//Dispose of the dialog
-		connectingToMyoDialog.setVisible(false);
-		connectingToMyoDialog.dispose();
+		//connectingDialog.setVisible(false);
+		//connectingDialog.dispose();
 		
 		myo.startHubThread(100);
 	}
